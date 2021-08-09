550770a670dfb3fb859db432101d1669f5211c9a
Emre OGUZ <e.oguz95@hotmail.com>
2021-08-09 14:46:10 +0300

    Initial commit.
    models created.

---
+++ src/dev/patika/Quixotic95/models/Course.java
@@ -0,0 +1,43 @@
+package dev.patika.Quixotic95.models;
+
+public class Course {
+
+    private int id;
+
+    private String courseName;
+    private int courseCode;
+    private double creditScore;
+
+    public Course() {
+    }
+
+    public Course(String courseName, int courseCode, double creditScore) {
+        this.courseName = courseName;
+        this.courseCode = courseCode;
+        this.creditScore = creditScore;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public int getCourseCode() {
+        return courseCode;
+    }
+
+    public void setCourseCode(int courseCode) {
+        this.courseCode = courseCode;
+    }
+
+    public double getCreditScore() {
+        return creditScore;
+    }
+
+    public void setCreditScore(double creditScore) {
+        this.creditScore = creditScore;
+    }
+}
---
+++ src/dev/patika/Quixotic95/models/Student.java
@@ -0,0 +1,55 @@
+package dev.patika.Quixotic95.models;
+
+import java.time.LocalDate;
+
+public class Student {
+
+    private int id;
+
+    private String name;
+    private LocalDate birthDate;
+    private String address;
+    private char gender;
+
+    public Student() {
+    }
+
+    public Student(String name, LocalDate birthDate, String address, char gender) {
+        this.name = name;
+        this.birthDate = birthDate;
+        this.address = address;
+        this.gender = gender;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public char getGender() {
+        return gender;
+    }
+
+    public void setGender(char gender) {
+        this.gender = gender;
+    }
+}
